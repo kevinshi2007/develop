76f4d21565997fc71d81bfd4bdf51690cdc97b12
Shi <Kevin.Shi@ontario.ca>
2018-02-22 21:54:43 -0500

    test1 localization and one button completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  currentLang = 'en';
+
+  constructor(private translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang(this.currentLang);
+
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use(this.currentLang);
+}
+
+switchLangEvent(event) {
+  if(this.currentLang == 'en')
+    this.currentLang = 'fr';
+  else
+    this.currentLang = 'en';
+
+    this.translate.use(this.currentLang);
+}
+
 }
